db59afb319ad0786c15b31fdb4bb6c3b357233bb
Liz <lizlie.holleza@awsys-i.com>
2018-07-05 13:26:16 +0800

    Update the word adapter to add the image view

--- app/src/main/java/com/example/lizlieholleza/miwok/WordAdapter.java
+++ app/src/main/java/com/example/lizlieholleza/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,6 +31,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView dTextView = (TextView)  listItemView.findViewById(R.id.default_text_view);
         dTextView.setText(currentWord.getDefaultWord());
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        imageView.setImageResource(currentWord.getImageId());
 
         return  listItemView;
     }
