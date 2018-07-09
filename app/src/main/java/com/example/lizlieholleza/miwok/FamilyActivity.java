9687d15ad1fa355e1db4477431fd1cbb01496a17
Liz <lizlie.holleza@awsys-i.com>
2018-07-09 17:54:56 +0800

    Update word constructor for family activity

--- app/src/main/java/com/example/lizlieholleza/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/lizlieholleza/miwok/FamilyActivity.java
@@ -15,16 +15,16 @@ public class    FamilyActivity extends AppCompatActivity {
 
         ArrayList<Word> familyList = new ArrayList<Word>();
 
-        familyList.add(new Word("fatherәpә", "father", R.drawable.family_father));
-        familyList.add(new Word("әṭa", "mother", R.drawable.family_mother));
-        familyList.add(new Word("angsi", "son", R.drawable.family_son));
-        familyList.add(new Word("tune", "daughter", R.drawable.family_daughter));
-        familyList.add(new Word("taachi", "older brother", R.drawable.family_older_brother));
-        familyList.add(new Word("chalitti", "younger brother", R.drawable.family_younger_brother));
-        familyList.add(new Word("teṭe", "older sister", R.drawable.family_older_sister));
-        familyList.add(new Word("kolliti", "younger sister",R.drawable.family_younger_sister));
-        familyList.add(new Word("ama", "grandmother", R.drawable.family_grandmother));
-        familyList.add(new Word("paapa", "grandfather", R.drawable.family_grandfather));
+        familyList.add(new Word("fatherәpә", "father", R.drawable.family_father, R.raw.family_father));
+        familyList.add(new Word("әṭa", "mother", R.drawable.family_mother, R.raw.family_mother));
+        familyList.add(new Word("angsi", "son", R.drawable.family_son, R.raw.family_son));
+        familyList.add(new Word("tune", "daughter", R.drawable.family_daughter, R.raw.family_daughter));
+        familyList.add(new Word("taachi", "older brother", R.drawable.family_older_brother, R.raw.family_older_brother));
+        familyList.add(new Word("chalitti", "younger brother", R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        familyList.add(new Word("teṭe", "older sister", R.drawable.family_older_sister, R.raw.family_older_sister));
+        familyList.add(new Word("kolliti", "younger sister",R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        familyList.add(new Word("ama", "grandmother", R.drawable.family_grandmother, R.raw.family_grandmother));
+        familyList.add(new Word("paapa", "grandfather", R.drawable.family_grandfather, R.raw.family_grandfather));
 
         WordAdapter adapter = new WordAdapter(this, familyList, R.color.category_family);
         ListView  lv = (ListView) findViewById(R.id.list);
