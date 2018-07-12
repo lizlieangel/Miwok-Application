a3bbee378821e66b45bcb8b667e568fe55d59714
Liz <lizlie.holleza@awsys-i.com>
2018-07-12 18:33:00 +0800

    transfer content from the oncreate method on the colors activity to oncreateview in colors fragment

--- app/src/main/java/com/example/lizlieholleza/miwok/ColorsFragment.java
+++ app/src/main/java/com/example/lizlieholleza/miwok/ColorsFragment.java
@@ -1,6 +1,7 @@
 package com.example.lizlieholleza.miwok;
 
 
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -8,7 +9,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,6 +51,35 @@ public class ColorsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
       View rootview = inflater.inflate(R.layout.word_list, container, false);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        final ArrayList<Word> colorList = new ArrayList<Word>();
+
+        colorList.add(new Word("weṭeṭṭi","red", R.drawable.color_red, R.raw.color_red));
+        colorList.add(new Word("chokokki","green", R.drawable.color_green, R.raw.color_green));
+        colorList.add(new Word("ṭakaakki","brown", R.drawable.color_brown, R.raw.color_brown));
+        colorList.add(new Word("ṭopoppi","gray", R.drawable.color_gray, R.raw.color_gray));
+        colorList.add(new Word("kululli","black", R.drawable.color_black, R.raw.color_black));
+        colorList.add(new Word("kelelli","white", R.drawable.color_white, R.raw.color_white));
+        colorList.add(new Word("ṭopiisә","dusty yellow", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        colorList.add(new Word("chiwiiṭә","mustard yellow", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
+
+        WordAdapter adapter = new WordAdapter(this, colorList, R.color.category_colors);
+        releaseMediaPlayer();
+        ListView lv = (ListView) findViewById(R.id.list);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                releaseMediaPlayer();
+                Word word = colorList.get(position);
+                int result = audioManager.requestAudioFocus(mAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getSoundId());
+                    mediaPlayer.start();
+                    mediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+        });
       return rootview;
     }
 
