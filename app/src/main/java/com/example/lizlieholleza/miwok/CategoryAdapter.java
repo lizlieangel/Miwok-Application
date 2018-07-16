038e7ccaf1c1b223a1e37ea400bb28c6d608ad45
Liz <lizlie.holleza@awsys-i.com>
2018-07-16 09:35:58 +0800

    Show the tabs

--- app/src/main/java/com/example/lizlieholleza/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/lizlieholleza/miwok/CategoryAdapter.java
@@ -1,12 +1,17 @@
 package com.example.lizlieholleza.miwok;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
-    public CategoryAdapter(FragmentManager fm) {
+    private Context context;
+
+    public CategoryAdapter(FragmentManager fm, Context context) {
         super(fm);
+        this.context = context;
     }
 
     @Override
@@ -28,5 +33,18 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position == 0) {
+            return context.getString(R.string.category_numbers);
+        } else if(position == 1) {
+            return context.getString(R.string.category_colors);
+        } else if(position == 2) {
+            return context.getString(R.string.category_family);
+        } else {
+            return context.getString(R.string.category_phrases);
+        }
     }
 }
--- app/src/main/java/com/example/lizlieholleza/miwok/MainActivity.java
+++ app/src/main/java/com/example/lizlieholleza/miwok/MainActivity.java
@@ -4,34 +4,17 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
-    TabLayout tabLayout;
-    ViewPager viewPager;
-    public static String POSITION = "POSITION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new MiwokFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
-        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        CategoryAdapter categoryAdapter = new CategoryAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(categoryAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-//        CategoryAdapter ca = new CategoryAdapter(getSupportFragmentManager());
-//        viewPager.setAdapter(ca);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(POSITION, tabLayout.getSelectedTabPosition());
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        viewPager.setCurrentItem(savedInstanceState.getInt(POSITION));
     }
 }
