6b139fca07b9a0a795ef6faac823d4e8fd84fa43
Liz <lizlie.holleza@awsys-i.com>
2018-07-16 08:47:04 +0800

    delete fragment xml and update activity main xml

--- app/src/main/java/com/example/lizlieholleza/miwok/MainActivity.java
+++ app/src/main/java/com/example/lizlieholleza/miwok/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.lizlieholleza.miwok;
 
-import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    public static String POSITION = "POSITION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        CategoryAdapter ca = new CategoryAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(ca);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MiwokFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+//        CategoryAdapter ca = new CategoryAdapter(getSupportFragmentManager());
+//        viewPager.setAdapter(ca);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(POSITION, tabLayout.getSelectedTabPosition());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        viewPager.setCurrentItem(savedInstanceState.getInt(POSITION));
     }
 }
